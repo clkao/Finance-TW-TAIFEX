b29d983e1d662a8994cdf1d4dc7b0d7e1e87f24b
Chia-liang Kao <clkao@clkao.org>
2011-05-01 17:34:29 +0800

    Checking in changes prior to tagging of version 0.34.  Changelog diff is:
    diff --git a/Changes b/Changes
    index fa0e846..9f1cad0 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Finance::TW::TAIFEX
    
    +0.34 Sun May  1 17:33:59 CST 2011
    +        - correct 2011 calendar.
    +
     0.33 Sun Feb  6 21:25:37 CST 2011
             - Better any-moose deps handling.
             - correct 2011 lunar new year calendar.

--- lib/Finance/TW/TAIFEX.pm
+++ lib/Finance/TW/TAIFEX.pm
@@ -12,7 +12,7 @@ use Finance::TW::TAIFEX::Product;
 use Finance::TW::TAIFEX::Contract;
 
 use 5.008_001;
-our $VERSION = '0.33';
+our $VERSION = '0.34';
 
 has context_date => ( is => "rw", isa => "DateTime",
                       default => sub { DateTime->now(time_zone => 'Asia/Taipei') },
